8b370816766012a41011cafdd9d544233fd8b505
tannuup123 <tanmayup123@gmail.com>
2025-09-21 18:18:09 +0530

    publish updates

--- my-excel-analytics-app/src/index.js
+++ my-excel-analytics-app/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
   <DarkModeProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/excel">
       {/* {" "} */}
       <App />
     </BrowserRouter>
